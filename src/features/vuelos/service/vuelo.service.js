a5fe2d0bb7bfd03ce639671b2fd89cd5d96d6843
ivogabreil19 <ivodegange@gmail.com>
2025-06-24 21:55:43 -0300

    adaptacion api local para vercell

--- src/features/vuelos/service/vuelo.service.js
+++ src/features/vuelos/service/vuelo.service.js
@@ -3,7 +3,8 @@ const TOKEN = "f75b8ddb82e8e5071987100a29dabab0";
 export async function obtenerVuelos(origen, destino) {
   try {
     const res = await fetch(
-      `http://localhost:3001/api/vuelos?origin=${origen}&destination=${destino}`,
+      //`http://localhost:3001/api/vuelos?origin=${origen}&destination=${destino}`,
+      `/api/vuelos?origin=${origen}&destination=${destino}`,
       {
         headers: {
           "X-Access-Token": TOKEN,
